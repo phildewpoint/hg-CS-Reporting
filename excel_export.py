da195c82080f3702b57c271ddce20d7600718023
Phil DeSantis <phildewpoint@gmail.com>
2017-08-31 13:41:44 -0500

    Added excel functions and GUI updates

--- excel_export.py
+++ excel_export.py
@@ -2,6 +2,7 @@ from appJar import gui
 import openpyxl
 import re
 import os
+import datetime
 buttons = [
     'Go!',
     'Cancel'
@@ -15,22 +16,41 @@ fileRegex = [
 def launch(button):
     if button == "Cancel":
         quit()
-    elif app.getEntry(name="find_dir") is not True:
+    elif not app.getEntry(name="find_dir"):
         msg = "You need to pick a directory to start the app."
         app.warningBox(title="ERROR: Missing Information", message=msg)
     else:
         direc = app.getEntry(name="find_dir")
+        # check the chosen directory for # of files
         entry = os.scandir(direc)
-        app.setLabel(name="file_count", text=("Files Located: " + str(len(entry))))
+        file_cnt = 0
+        filelist = []
+        for i in entry:
+            if not i.name.startswith('.') and i.is_file():
+                file_cnt += 1
+                filelist.append(i.name)
+        app.setLabel(name="file_count", text=("Files Located: " + str(file_cnt)))
+        # create workbook
         wb = openpyxl.Workbook()
-        dest_wb = "PJD_Test_Workbook.xlsx"
-        wb2 = openpyxl.load_workbook(direc.join('test.xlsx'))
-        print(wb2.sheetnames)
+        # copy/paste from other xlsx into new file
+        for excel in filelist:
+            load_wb = openpyxl.load_workbook(os.path.join(direc, excel))
+            copy_sheet = load_wb.active
+            rows = []
+            for row in copy_sheet.iter_rows():
+                row_data = []
+                for cell in row:
+                    row_data.append(cell.value)
+
+        wb.save(filename=(os.path.join(direc, "CS_Compiled_Reports" + str(datetime.date.today()) + ".xlsx")))
+        app.addMessage(title="Workbook Complete!", text="Your workbook is complete.")
+        quit()
 
 
 def run_app():
-    msg = "Runs calculations over multiple spreadsheets. Select where all the source spreadsheets are stored and GO!"
-    app.addLabel(title="header", text=msg)
+    msg = "Runs calculations over multiple spreadsheets. Select where all the source spreadsheets are stored.\n"
+    msg2 = "A spreadsheet will be created in that folder combining this data."
+    app.addLabel(title="header", text=msg + msg2)
     app.addHorizontalSeparator()
     app.addDirectoryEntry(title="find_dir", )
     app.addLabel(title="file_count", text="Files Located: 0")
